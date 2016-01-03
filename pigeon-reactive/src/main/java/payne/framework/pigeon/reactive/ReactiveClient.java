72df5292618893fa17b3a17d464b1736cf53d8be
core-lib <yangchangpei@yangchaeidemini.lan>
2016-01-03 16:52:16 +0800

    添加一个ReactiveClient避免不能maven编译

---
+++ pigeon-reactive/src/main/java/payne/framework/pigeon/reactive/ReactiveClient.java
@@ -0,0 +1,21 @@
+package payne.framework.pigeon.reactive;  
+
+/** 
+ * <p> 
+ * Description: 
+ * </p> 
+ *  
+ * <p> 
+ * Company: 广州市俏狐信息科技有限公司 
+ * </p> 
+ *  
+ * @author yangchangpei 646742615@qq.com
+ *
+ * @date 2016年1月3日 下午4:43:17
+ *
+ * @version 1.0.0 
+ */
+public class ReactiveClient {
+
+}
+ 
