307598e99b5c55f470c7fac906cf51633d9ad61f
YangChangPei <646742615@qq.com>
2018-03-06 09:45:04 +0800

    调整 Async 接口的生成格式

--- pigeon-generation-async/src/test/java/payne/framework/pigeon/generation/async/SampleAPI.java
+++ pigeon-generation-async/src/test/java/payne/framework/pigeon/generation/async/SampleAPI.java
@@ -7,21 +7,22 @@ import payne.framework.pigeon.generation.annotation.Name;
  * <p>
  * Description:
  * </p>
- * 
+ * <p>
  * <p>
  * Company: 广州市俏狐信息科技有限公司
  * </p>
- * 
+ *
  * @author yangchangpei 646742615@qq.com
- *
- * @date 2016年1月8日 下午9:06:41
- *
  * @version 1.0.0
+ * @date 2016年1月8日 下午9:06:41
  */
 @Open("/sample")
 public interface SampleAPI {
 
-	@Open("/login")
-	User login(@Name("username") String username, @Name("password") String password) throws Exception;
+    @Open("/login")
+    User login(@Name("username") String username, @Name("password") String password) throws Exception;
+
+    @Open("/logout")
+    void logout() throws Exception;
 
 }
