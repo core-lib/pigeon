8994ad6118324979f287b07698650adfe09aa822
YangChangPei <646742615@qq.com>
2018-03-15 10:21:54 +0800

    Reactive 接口生成带源码注释

--- pigeon-generation-reactive/src/test/java/payne/framework/pigeon/generation/reactive/Tests.java
+++ pigeon-generation-reactive/src/test/java/payne/framework/pigeon/generation/reactive/Tests.java
@@ -1,11 +1,10 @@
 package payne.framework.pigeon.generation.reactive;
 
-import java.io.File;
-
 import org.junit.Test;
-
 import payne.framework.pigeon.generation.Generator;
 import payne.framework.pigeon.generation.Interface;
+
+import java.io.File;
 
 /**
  * <p>
@@ -27,9 +26,7 @@ public class Tests {
 	@Test
 	public void testGenerate() throws Exception {
 		File directory = new File(System.getProperty("java.io.tmpdir") + "reactive");
-		if (directory.exists() == false) {
-			directory.mkdirs();
-		}
+		if (!directory.exists() && !directory.mkdirs()) throw new IllegalStateException("创建文件夹失败");
 		Generator generator = new ReactiveGenerator(directory);
 		generator.generate(new Interface("/", SampleAPI.class));
 		System.out.println(directory);
